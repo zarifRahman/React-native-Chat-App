418a3ae3ff699ea2e1307c91a60b31a589ffda14
Zarif Shahriar <zarif.rahman@circlefintech.com>
2021-05-21 15:03:26 +0600

    add: project

---
+++ App.tsx
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Chat from './components/Chat';
+import PersonalInfo from './components/PersonalInfo';
+import Styles from './components/Styles';
+
+export default function App() {
+  const storageUserNameKey = 'chatapp-username';
+  const storageImageKey = 'chatapp-image';
+  const [username, setUsername] = useState('');
+  const [image, setImage] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchPersonalData = async () => {
+    let fetchedUsername = await AsyncStorage.getItem(storageUserNameKey);
+    let userName = fetchedUsername == null ? '' : fetchedUsername;
+    let fetchedImage = await AsyncStorage.getItem(storageImageKey);
+    let image = fetchedImage == null ? '' : fetchedImage;
+    setUsername(userName);
+    setImage(image);
+  };
+  useEffect(() => {
+    fetchPersonalData;
+  }, []);
+
+  const onPersonalInfoClosed = async (name: string, image: string) => {
+    setUsername(name);
+    await AsyncStorage.setItem(storageUserNameKey, name);
+    setImage(image);
+    await AsyncStorage.setItem(storageImageKey, image);
+  };
+  let activeComponent =
+    username != '' ? (
+      <Chat username={username} image={image} />
+    ) : (
+      <PersonalInfo onClosed={onPersonalInfoClosed} />
+    );
+  return (
+    <SafeAreaView style={Styles.container}>
+      {activeComponent}
+      <StatusBar backgroundColor="green" barStyle="dark-content" />
+    </SafeAreaView>
+  );
+}
---
+++ components/PersonalInfo.tsx
@@ -0,0 +1,88 @@
+import React, {useState} from 'react';
+import {View, TextInput, Button, Image, Text, Alert} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
+// import ImgToBase64 from 'react-native-image-base64';
+// import {CropView} from 'react-native-image-crop-tools';
+import Styles from './Styles';
+
+type PersonalInfoProps = {
+  onClosed: (name: string, image: any) => void;
+};
+
+const PersonalInfo = ({onClosed}: PersonalInfoProps) => {
+  const [name, setName] = useState('');
+  const [imageSource, setImageSource] = useState(null);
+  const selectImage = () => {
+    let options: any = {
+      title: 'You can choose one image',
+      maxWidth: 256,
+      maxHeight: 256,
+      noData: true,
+      mediaType: 'photo',
+      storageOptions: {
+        skipBackup: true,
+      },
+    };
+    launchImageLibrary(options, (response: any) => {
+      if (response.didCancel) {
+        console.log('User cancelled photo picker');
+        Alert.alert('You did not select any image');
+      } else if (response?.error) {
+        console.log('ImagePicker Error: ', response?.error);
+      } else if (response?.customButton) {
+        console.log('User tapped custom button: ', response?.customButton);
+      } else {
+        let source = {uri: response.uri};
+
+        // ADD THIS
+        setImageSource(source.uri);
+        // setImageData(source.uri);
+      }
+    });
+  };
+
+  return (
+    <View style={Styles.personalInfoContainer}>
+      <Image
+        style={Styles.logo}
+        source={require('../assets/wired-brain-coffee-logo.png')}
+      />
+      <View style={Styles.enterYourName}>
+        <Text style={Styles.nameText}>Please enter your name</Text>
+        <TextInput
+          style={Styles.nameTextInput}
+          onChangeText={(text) => setName(text)}
+          value={name}
+        />
+      </View>
+      <View style={{alignItems: 'center'}}>
+        {imageSource === null ? (
+          <Image
+            source={require('../assets/wired-brain-coffee-logo.png')}
+            style={Styles.imageBox}
+            resizeMode="contain"
+          />
+        ) : (
+          <Image
+            source={
+              imageSource
+                ? {uri: imageSource}
+                : require('../assets/wired-brain-coffee-logo.png')
+            }
+            style={Styles.imageBox}
+            resizeMode="contain"
+          />
+        )}
+      </View>
+
+      {/* <ImageChooser onChangeImage={image => setImage(image)} /> */}
+      <Button title="Upload Image" onPress={selectImage} />
+      <Button
+        title="Start chatting!"
+        onPress={() => onClosed(name, imageSource)}
+      />
+    </View>
+  );
+};
+
+export default PersonalInfo;
